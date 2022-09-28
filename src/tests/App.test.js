95a3fa2c66b5c1cf0d5b5cfaae4b9404224beed0
ItzMari0 <msajr@outlook.com>
2022-09-27 21:43:44 -0300

    [Grupo 25] Teste do Footer e reftoração

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -2,15 +2,12 @@ import React from 'react';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from '../App';
-import Header from '../components/Header';
 import renderWithRouter from './utils/renderWithRouter';
 
 const EMAIL_ID = 'email-input';
 const PASSWORD_ID = 'password-input';
 const LOGIN_BTN_ID = 'login-submit-btn';
 const EMAIL = 'grupo25@trybe.com';
-const PROFILE_ICON_ID = 'profile-top-btn';
-const SEARCH_ICON_ID = 'search-top-btn';
 
 describe('App test coverage', () => {
   describe('Login page coverage', () => {
@@ -55,66 +52,6 @@ describe('App test coverage', () => {
       const loginBtn = screen.getByTestId(LOGIN_BTN_ID);
       userEvent.click(loginBtn);
       expect(history.location.pathname).toBe('/meals');
-    });
-  });
-
-  describe('Header component coverage', () => {
-    it('redirects to Profile from Meals Page after clicking on Profile Icon, Search icon/bar behavior', async () => {
-      const { history } = renderWithRouter(<Header />, '/meals');
-
-      const headerTitle = screen.getByTestId('page-title');
-      expect(headerTitle).toBeInTheDocument();
-      expect(headerTitle).toHaveTextContent(/Meals/i);
-
-      const profileBtn = screen.queryByTestId(PROFILE_ICON_ID);
-      expect(profileBtn).toBeInTheDocument();
-      const searchBtn = screen.queryByTestId(SEARCH_ICON_ID);
-      expect(searchBtn).toBeInTheDocument();
-
-      userEvent.click(searchBtn);
-      const searchBar = screen.getByTestId('search-input');
-      expect(searchBar).toBeInTheDocument();
-
-      userEvent.click(profileBtn);
-      expect(history.location.pathname).toBe('/profile');
-      expect(searchBtn).not.toBeInTheDocument();
-    });
-
-    it('Header behavior on Drinks page', () => {
-      renderWithRouter('/drinks');
-
-      const headerTitle = screen.getByTestId('page-title');
-      expect(headerTitle).toBeInTheDocument();
-      expect(headerTitle).toHaveTextContent(/Drinks/i);
-
-      const profileBtn = screen.queryByTestId(PROFILE_ICON_ID);
-      expect(profileBtn).toBeInTheDocument();
-      const searchBtn = screen.queryByTestId(SEARCH_ICON_ID);
-      expect(searchBtn).toBeInTheDocument();
-    });
-
-    it('Header behavior on Done Recipes page', () => {
-      renderWithRouter('/done-recipes');
-
-      const headerTitle = screen.getByText(/Done Recipes/i);
-      expect(headerTitle).toBeInTheDocument();
-
-      const profileBtn = screen.queryByTestId(PROFILE_ICON_ID);
-      expect(profileBtn).toBeInTheDocument();
-      const searchBtn = screen.queryByTestId(SEARCH_ICON_ID);
-      expect(searchBtn).not.toBeInTheDocument();
-    });
-
-    it('Header behavior on Favorite Recipes page', () => {
-      renderWithRouter('/favorite-recipes');
-
-      const headerTitle = screen.getByText(/Favorite Recipes/i);
-      expect(headerTitle).toBeInTheDocument();
-
-      const profileBtn = screen.queryByTestId(PROFILE_ICON_ID);
-      expect(profileBtn).toBeInTheDocument();
-      const searchBtn = screen.queryByTestId(SEARCH_ICON_ID);
-      expect(searchBtn).not.toBeInTheDocument();
     });
   });
 });
---
+++ src/tests/Footer.test.js
@@ -0,0 +1,25 @@
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './utils/renderWithRouter';
+
+describe('Footer component coverage', () => {
+  it('redirects to Drinks page after clicking on Drinks Footer Icon', async () => {
+    const { history } = renderWithRouter('/meals');
+
+    const drinksFooterBtn = screen.getByTestId('drinks-bottom-btn');
+    expect(drinksFooterBtn).toBeInTheDocument();
+      
+    userEvent.click(drinksFooterBtn);
+    expect(history.location.pathname).toBe('/drinks');
+  });
+
+  it('redirects to Meals page after clicking on Meals Footer Icon', async () => {
+    const { history } = renderWithRouter('/drinks');
+
+    const mealsFooterBtn = screen.getByTestId('meals-bottom-btn');
+    expect(mealsFooterBtn).toBeInTheDocument();
+      
+    userEvent.click(mealsFooterBtn);
+    expect(history.location.pathname).toBe('/meals');
+  });
+});
